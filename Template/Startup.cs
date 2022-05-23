d08f6790459dccb374f76b12b402ab0778f6aeb8
Arnaldo Sena <arsena68@gmail.com>
2022-05-23 16:16:23 -0300

    Instalando e Configurando Entity Framework Core em um projeto .Net Core

--- Template/Startup.cs
+++ Template/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -10,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Template.Data.Context;
 
 namespace Template
 {
@@ -25,6 +27,9 @@ namespace Template
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<TemplateContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("TemplateDB")).EnableSensitiveDataLogging());
+            //NativeInjector.RegisterServices(services);
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
