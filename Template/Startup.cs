2634ce446815aeb5bb84d713bb5f5165e22d7891
Arnaldo Sena <arsena68@gmail.com>
2022-05-24 18:16:29 -0300

    Teste

--- Template/Startup.cs
+++ Template/Startup.cs
@@ -25,7 +25,7 @@ namespace Template
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container.2
         public void ConfigureServices(IServiceCollection services)
         {
 
